aac975b9d10095ab3c37b0b0e0d79fe4507da7c1
Michał Piotr Stankiewicz <ms335789@students.mimuw.edu.pl>
2018-06-27 12:00:35 +0200

    Add pytest as test environment

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python3
+
 from distutils.core import setup
+import setuptools
+
+setup_requires = [
+    'pytest-runner',
+]
+
+tests_require = [
+    'pytest',
+]
 
 setup(
     name='lattec',
     version='0.1dev',
     packages=['lattec'],
+    package_dir={'':'src'},
     long_description=open('README.rst').read(),
+    setup_requires=setup_requires,
+    tests_require=tests_require,
 )
 
